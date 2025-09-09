bade2b906fb54d6dbda7f73394bbea104e3f347d
EstherMutua <esthermutua16@gmail.com>
2025-09-10 00:00:13 +0300

    added gunicorn

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
 import { Toaster } from 'react-hot-toast';
-import './index.css'; // Optional: if you have global styles
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
